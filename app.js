dbc22c3ad0920e6b71e1cb484ac9582026364b54
Djassem <48794909+GhosTRyPa@users.noreply.github.com>
2026-02-19 23:44:30 +0100

    aah

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log("Hello World");
+// function getComputerChoice() {
+//   let pcchoice = Math.floor(Math.random() * 3);
+//   switch (pcchoice) {
+//     case 0:
+//       return "paper";
+//       break;
+//     case 1:
+//       return "rock";
+//       break;
+//     case 2:
+//       return "scissors";
+//       break;
+//   }
+// }
+// let computerChoice = getComputerChoice(); // here i got the computer choice
+let computerChoice = "rock";
+// -------------------------------------------------------------------
+
+function getHumanChoice() {
+  let humanInput = prompt("Rock, Paper, Scissors ?");
+  return humanInput.toLowerCase();
+}
+let humanChoice = getHumanChoice(); // here i got the human choice
+
+// -------------------------------------------------------------------
+
+// let humanScore = 0;
+// let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return "Its a tie!";
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return `you won !, ${humanChoice} beats ${computerChoice}`;
+  } else {
+    return `the computer won!, ${computerChoice} beats ${humanChoice}`;
+  }
+}
+console.log(playRound(humanChoice, computerChoice));
